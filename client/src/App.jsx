1a761c69a7ed85cfda3678f25d7ef677aacafc78
Yogaraj-k <yogu0713@gmail.com>
2024-02-20 15:59:43 +0530

    offer page's stylinf fixed, mywallet is under process

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,9 @@ import Dashboard from "./components/admin/dashboard";
 import Offers from "./pages/user/offers";
 
 import logo from "./assets/images/logo.png";
-import offersBanner from "./assets/images/offers-image/single-banner.jpg";
+import banner from "./assets/images/offers-image/single-banner.jpg";
 import offerImage from "./assets/images/offers-image/11.jpg";
+import MyWallet from "./pages/user/myWallet";
 
 function App() {
 	const categories = [
@@ -26,10 +27,17 @@ function App() {
 			group: "Groceries",
 		},
 	];
+
 	const offersData = {
-		bannerImage: offersBanner,
+		bannerImage: banner,
 		offerImage: offerImage,
+		offerTitle: "Diwali24",
 	};
+
+	const walletData = {
+		bannerImage: banner,
+	};
+
 	return (
 		<div className="App">
 			<Router>
@@ -42,6 +50,7 @@ function App() {
 					<Route path="/login" element={<Login />} />
 					<Route path="/changePassword" element={<ChangePassword />} />
 					<Route path="/offers" element={<Offers offerData={offersData}></Offers>}></Route>
+					<Route path="/myWallet" element={<MyWallet walletData={walletData}></MyWallet>}></Route>
 
 					{/* adminpanel */}
 					<Route path="/addcategory" element={<AddCategory></AddCategory>}></Route>
