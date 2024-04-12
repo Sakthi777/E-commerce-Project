a6bcbd44e0a901f49c45b3001138aff73e3c965b
Segolsysit <segolsys@gmail.com>
2024-04-12 15:54:24 +0530

    Addcard Updated

--- server/routes/userDetailsRoute.js
+++ server/routes/userDetailsRoute.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const userDetailsDatas = express.Router();
-const { postAddToCardDetailsControllers, getAddToCardDetailsControllers } = require("../controllers/userDetailsControllers");
+const { postAddToCardDetailsControllers, getAddToCardDetailsControllers, IncrementAddToCartProductQuantity, DecrementAddToCartProductQuantity } = require("../controllers/userDetailsControllers");
 
 userDetailsDatas.post("/post-AddToCardDetails", postAddToCardDetailsControllers);
 
-userDetailsDatas.get("/get-userDetails", getAddToCardDetailsControllers);
+userDetailsDatas.get("/get-userCartDetails/:userID", getAddToCardDetailsControllers);
+
+userDetailsDatas.put("/IncrementAddToCartProductQuantity/:productID/:userID", IncrementAddToCartProductQuantity);
+
+userDetailsDatas.put("/DecrementAddToCartProductQuantity/:productID/:userID", DecrementAddToCartProductQuantity);
 
 module.exports = userDetailsDatas;
