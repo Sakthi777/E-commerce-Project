6cafc819d5bed5f05c6efe2782c14190ca3d8a14
Segolsysit <segolsys@gmail.com>
2024-04-24 14:42:01 +0530

    Order route

--- server/controllers/orderDetailsController.js
+++ server/controllers/orderDetailsController.js
@@ -2,33 +2,47 @@ const orderDetails = require("../models/OrderDetailsSchema");
 const asyncHandler = require("../middlewares/catchAsyncError");
 
 exports.postOrderDetailsControllers = async (req, res) => {
-  console.log(req.body);
-  try {
-    const { orderId, amount, paymentDate, productDetails, userDetails } = req.body;
-    const { id } = req.user;
-    // Use Order.create to create and save a new Order document
-    const Order = await orderDetails.create({
-      token: id,
-      orderId,
-      amount,
-      paymentDate: new Date(paymentDate),
-      productDetails,
-      userDetails,
-    });
+	console.log(req.body);
+	try {
+		const { orderId, amount, paymentDate, productDetails, userDetails } = req.body;
+		const { id } = req.user;
+		// Use Order.create to create and save a new Order document
+		const Order = await orderDetails.create({
+			token: id,
+			orderId,
+			amount,
+			paymentDate: new Date(paymentDate),
+			productDetails,
+			userDetails,
+		});
 
-    res.send(Order);
-  } catch (error) {
-    console.error("Error saving order details:", error);
-    res.status(500).json({ error: "Failed to save order details" });
-  }
+		res.send(Order);
+	} catch (error) {
+		console.error("Error saving order details:", error);
+		res.status(500).json({ error: "Failed to save order details" });
+	}
 };
 
 exports.getOrderDetailsController = async (req, res) => {
-  try {
-    const orders = await orderDetails.find();
-    res.send(orders);
-  } catch (error) {
-    console.error("Error fetching orders:", error);
-    res.status(500).json({ error: "Failed to fetch orders" });
-  }
+	try {
+		const orders = await orderDetails.find();
+		console.log(id);
+
+		await res.send(orders);
+	} catch (error) {
+		console.error("Error fetching orders:", error);
+		res.status(500).json({ error: "Failed to fetch orders" });
+	}
 };
+
+exports.getSingleOrderDetailController = asyncHandler(async (req, res, next) => {
+	try {
+		const { id } = req.userParams;
+		const orders = await orderDetails.find({ token: id });
+		console.log(orders.data);
+		res.send(id);
+	} catch (error) {
+		console.error("Error fetching orders:", error);
+		res.status(500).json({ error: "Failed to fetch orders" });
+	}
+});
--- server/routes/orderDetailsRoute.js
+++ server/routes/orderDetailsRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const orderDetailsDatas = express.Router();
-const { authenticate } = require("../middlewares/authMiddleWare");
-const { postOrderDetailsControllers, getOrderDetailsController } = require("../controllers/orderDetailsController");
+const { authenticate, authorized, authenticateParams } = require("../middlewares/authMiddleWare");
+const { postOrderDetailsControllers, getOrderDetailsController, getSingleOrderDetailController } = require("../controllers/orderDetailsController");
 
 orderDetailsDatas.post("/orderDetails", authenticate, postOrderDetailsControllers);
 orderDetailsDatas.get("/getOrderDetails", getOrderDetailsController);
+orderDetailsDatas.get("/getOrderDetails/:token", authenticateParams, getSingleOrderDetailController);
+// getSingleOrderDetailController;
 
 module.exports = orderDetailsDatas;
