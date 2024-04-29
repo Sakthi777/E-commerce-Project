9d0b96eacb002031657951e6b101584a1deb0088
Yogaraj K <yogu0713@gmail.com>
2024-04-29 17:08:28 +0530

    order history

--- client/src/pages/user/OrderHistory.jsx
+++ client/src/pages/user/OrderHistory.jsx
@@ -16,6 +16,8 @@ import { useSelector } from "react-redux";
 export default function OrderHistory() {
 	const token = useSelector((state) => state.tokenDetails.token);
 	const [orderHist, setOrderHist] = useState([]);
+	const [productId, setProductId] = useState([]);
+	const [clickedIndex, setClickedIndex] = useState(0);
 	const [productDetails, setProductDetails] = useState([]);
 	const url = "http://localhost:8000";
 	const fetchOrderHistory = async () => {
@@ -26,39 +28,42 @@ export default function OrderHistory() {
 		fetchOrderHistory();
 	}, []);
 
-	const fetchProducts = async (products, index) => {
-		const id = products.productdetail;
-		// console.log(id);
-		const res = await axios.get(`${url}/get-userDetails/${id}`);
-		const productDet = res.data;
-		return (
-			<tr>
-				<td>
-					<h6>{index}</h6>
-				</td>
-				<td className="tablename">
-					<img src={`${url}/productImage/${productDet.image}`} alt="" />
-				</td>
-				<td>
-					<h6>{productDet.productName}</h6>
-				</td>
-				<td>
-					<h6>$19/kilo</h6>
-				</td>
-				<td>
-					<h6>Fresh Company</h6>
-				</td>
-				<td>
-					<h6>{products.quantity}</h6>
-				</td>
-			</tr>
-		);
-	};
+	useEffect(() => {
+		if (orderHist[0]) {
+			setProductId(orderHist[0].productDetails);
+		}
+	}, [orderHist]);
+
+	useEffect(() => {
+		const fetchProductDetails = async () => {
+			const uniqueProductIds = new Set(productId.map((prodDet) => prodDet.productdetail));
+			const uniqueProductDetails = [];
+
+			for (const id of uniqueProductIds) {
+				const res = await axios.get(`${url}/get-userDetails/${id}`);
+				const productDet = res.data.data;
+				uniqueProductDetails.push(productDet);
+			}
+
+			setProductDetails(uniqueProductDetails);
+		};
+		console.log(productId);
+		fetchProductDetails();
+	}, [productId]);
+
+	// useEffect(() => {
+	// 	console.log(productDetails);
+	// }, [productDetails]);
 
 	const renderOrderHistory = () => {
 		return orderHist.map((order, index) => {
 			return (
-				<div className="list-of-order">
+				<div
+					className="list-of-order"
+					onClick={() => {
+						setClickedIndex(index);
+						setProductId(order.productDetails);
+					}}>
 					<div className="order-list">
 						<div>
 							<div className="order1">
@@ -85,172 +90,199 @@ export default function OrderHistory() {
 									<span>Order Delivered</span>
 								</li>
 							</div>
-							<div className="delivary-list">
-								<div className="order-details">
-									<li>
-										<h6>Order ID</h6>
-										<p>14667</p>
-									</li>
-									<li>
-										<h6>Totl Item</h6>
-										<p>{order.productDetails.length} Items</p>
-									</li>
-									<li>
-										<h6>Order Time</h6>
-										<p>{order.paymentDate.slice(0, 10)}</p>
-									</li>
-									<li>
-										<h6>Delivery Time</h6>
-										<p>{order.paymentDate.slice(0, 10)}</p>
-									</li>
-								</div>
-								<div className="order-details">
-									<li>
-										<h6>Sub Total</h6>
-										<p>Rs.{order.amount}</p>
-									</li>
-									<li>
-										<h6>Discount</h6>
-										<p>Rs.0</p>
-									</li>
-									<li>
-										<h6>Delivery Fee</h6>
-										<p>Rs.0</p>
-									</li>
-									<li>
-										<h6>Total(Incl. VAT)</h6>
-										<p>Rs.{order.amount}</p>
-									</li>
-								</div>
-								<div className="order-address">
-									<h6>Delivery Location</h6>
-									<p>{order.address}</p>
-								</div>
-							</div>
-							<div className="registerAdmindata1">
-								<div className="table-row1">
-									<div className="table-listdata1">
-										<table>
-											<thead className="tablehead">
-												<tr>
-													<th>Serial</th>
-													<th>Product</th>
-													<th>Name</th>
-													<th>Price</th>
-													<th>Brand</th>
-													<th>Quantity</th>
-												</tr>
-											</thead>
-											<tbody className="tablebody">
-												{order &&
-													order.productDetails.forEach((product, index) => {
-														fetchProducts(product, index + 1);
-													})}
-												{/* <tr>
-													<td>
-														<h6>01</h6>
-													</td>
-													<td className="tablename">
-														<img src={product1} alt="" />
-													</td>
-													<td>
-														<h6>Product Name</h6>
-													</td>
-													<td>
-														<h6>$19/kilo</h6>
-													</td>
-													<td>
-														<h6>Fresh Company</h6>
-													</td>
-													<td>
-														<h6>3</h6>
-													</td>
-												</tr>
-												<tr>
-													<td>
-														<h6>02</h6>
-													</td>
-													<td className="tablename">
-														<img src={product2} alt="" />
-													</td>
-													<td>
-														<h6>Product Name</h6>
-													</td>
-													<td>
-														<h6>$19/kilo</h6>
-													</td>
-													<td>
-														<h6>Fresh Company</h6>
-													</td>
-													<td>
-														<h6>3</h6>
-													</td>
-												</tr>
-												<tr>
-													<td>
-														<h6>03</h6>
-													</td>
-													<td className="tablename">
-														<img src={product3} alt="" />
-													</td>
-													<td>
-														<h6>Product Name</h6>
-													</td>
-													<td>
-														<h6>$19/kilo</h6>
-													</td>
-													<td>
-														<h6>Fresh Company</h6>
-													</td>
-													<td>
-														<h6>3</h6>
-													</td>
-												</tr>
-												<tr>
-													<td>
-														<h6>04</h6>
-													</td>
-													<td className="tablename">
-														<img src={product4} alt="" />
-													</td>
-													<td>
-														<h6>Product Name</h6>
-													</td>
-													<td>
-														<h6>$19/kilo</h6>
-													</td>
-													<td>
-														<h6>Fresh Company</h6>
-													</td>
-													<td>
-														<h6>3</h6>
-													</td>
-												</tr>
-												<tr>
-													<td>
-														<h6>05</h6>
-													</td>
-													<td className="tablename">
-														<img src={product5} alt="" />
-													</td>
-													<td>
-														<h6>Product Name</h6>
-													</td>
-													<td>
-														<h6>$19/kilo</h6>
-													</td>
-													<td>
-														<h6>Fresh Company</h6>
-													</td>
-													<td>
-														<h6>3</h6>
-													</td>
-												</tr> */}
-											</tbody>
-										</table>
+							{clickedIndex === index && (
+								<div className="delivary-list">
+									<div className="order-details">
+										<li>
+											<h6>Order ID</h6>
+											<p>14667</p>
+										</li>
+										<li>
+											<h6>Totl Item</h6>
+											<p>{order.productDetails.length} Items</p>
+										</li>
+										<li>
+											<h6>Order Time</h6>
+											<p>{order.paymentDate.slice(0, 10)}</p>
+										</li>
+										<li>
+											<h6>Delivery Time</h6>
+											<p>{order.paymentDate.slice(0, 10)}</p>
+										</li>
+									</div>
+									<div className="order-details">
+										<li>
+											<h6>Sub Total</h6>
+											<p>Rs.{order.amount}</p>
+										</li>
+										<li>
+											<h6>Discount</h6>
+											<p>Rs.0</p>
+										</li>
+										<li>
+											<h6>Delivery Fee</h6>
+											<p>Rs.0</p>
+										</li>
+										<li>
+											<h6>Total(Incl. VAT)</h6>
+											<p>Rs.{order.amount}</p>
+										</li>
+									</div>
+									<div className="order-address">
+										<h6>Delivery Location</h6>
+										<p>{order.address}</p>
 									</div>
 								</div>
-							</div>
+							)}
+							{clickedIndex === index && (
+								<div className="registerAdmindata1">
+									<div className="table-row1">
+										<div className="table-listdata1">
+											<table>
+												<thead className="tablehead">
+													<tr>
+														<th>Serial</th>
+														<th>Product</th>
+														<th>Name</th>
+														<th>Price</th>
+														<th>Brand</th>
+														<th>Quantity</th>
+													</tr>
+												</thead>
+												<tbody className="tablebody">
+													{productDetails &&
+														productDetails.map((product, index) => {
+															const quantity = productId[index] ? productId[index].quantity : 0;
+
+															return (
+																<tr>
+																	<td>
+																		<h6>{index + 1}</h6>
+																	</td>
+																	<td className="tablename">
+																		<img src={product1} alt="" />
+																	</td>
+																	<td>
+																		<h6>{product.productName}</h6>
+																	</td>
+																	<td>
+																		<h6>$19/kilo</h6>
+																	</td>
+																	<td>
+																		<h6>Fresh Company</h6>
+																	</td>
+																	<td>
+																		<h6>{quantity}</h6>
+																	</td>
+																</tr>
+															);
+														})}
+													{/* <tr>
+												<td>
+													<h6>01</h6>
+												</td>
+												<td className="tablename">
+													<img src={product1} alt="" />
+												</td>
+												<td>
+													<h6>Product Name</h6>
+												</td>
+												<td>
+													<h6>$19/kilo</h6>
+												</td>
+												<td>
+													<h6>Fresh Company</h6>
+												</td>
+												<td>
+													<h6>3</h6>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<h6>02</h6>
+												</td>
+												<td className="tablename">
+													<img src={product2} alt="" />
+												</td>
+												<td>
+													<h6>Product Name</h6>
+												</td>
+												<td>
+													<h6>$19/kilo</h6>
+												</td>
+												<td>
+													<h6>Fresh Company</h6>
+												</td>
+												<td>
+													<h6>3</h6>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<h6>03</h6>
+												</td>
+												<td className="tablename">
+													<img src={product3} alt="" />
+												</td>
+												<td>
+													<h6>Product Name</h6>
+												</td>
+												<td>
+													<h6>$19/kilo</h6>
+												</td>
+												<td>
+													<h6>Fresh Company</h6>
+												</td>
+												<td>
+													<h6>3</h6>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<h6>04</h6>
+												</td>
+												<td className="tablename">
+													<img src={product4} alt="" />
+												</td>
+												<td>
+													<h6>Product Name</h6>
+												</td>
+												<td>
+													<h6>$19/kilo</h6>
+												</td>
+												<td>
+													<h6>Fresh Company</h6>
+												</td>
+												<td>
+													<h6>3</h6>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<h6>05</h6>
+												</td>
+												<td className="tablename">
+													<img src={product5} alt="" />
+												</td>
+												<td>
+													<h6>Product Name</h6>
+												</td>
+												<td>
+													<h6>$19/kilo</h6>
+												</td>
+												<td>
+													<h6>Fresh Company</h6>
+												</td>
+												<td>
+													<h6>3</h6>
+												</td>
+											</tr> */}
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+							)}
 						</div>
 					</div>
 				</div>
